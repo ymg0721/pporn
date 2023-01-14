038987e0d13facf7a57a6faec0df9919826fc08c
ymg0721 <yonamasahiro@gmail.com>
2023-01-14 15:39:14 +0900

    RSS取得確認データ追加

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var FeedParser = require('feedparser');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var data = [];
+  var feed = request('https://liginc.co.jp/feed/')
+  var feedparser = new FeedParser();
+
+  feed.on('error', function (error) {
+    // request エラー処理
+  });
+
+  feed.on('response', function (feed_res) {
+    var stream = this;
+    if (feed_res.statusCode !== 200) {
+      this.emit('error', new Error('Bad status code'));
+    }
+    else {
+      stream.pipe(feedparser);
+    }
+  });
+
+  feedparser.on('error', function (error) {
+    // feedparser エラー処理
+  });
+
+  feedparser.on('readable', function () {
+    var item;
+    while (item = this.read()) {
+      data.push({
+        'title': item.title,
+        'url': item.link
+      });
+    }
+  });
+
+  feedparser.on('end', function () {
+    res.render('index', { title: 'LIG Feed', data: data});
+  });
+
+  // res.render('index', { title: 'Express' }); //今回は使わないのでコメントアウト
 });
 
 module.exports = router;
